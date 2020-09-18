a348a01faf07371590c0b338b811ee148cb67a7e
Андрей <@DESKTOP-OKJ9O5F>
2020-09-18 21:33:59 +0300

    feat: create solutions and completed Tusk 3

---
+++ src/TeachMeSkills.DotNet.Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const double pi = 3.141592653;
+
+            double r = 33;
+            double result = (pi * r);
+            Console.WriteLine($"S circle = {result * result}");
+        }
+    }
+}
