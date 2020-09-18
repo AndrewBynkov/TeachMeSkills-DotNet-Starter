ddfaf240a1c33658f5c11f931dc402c94a3ffb2a
Андрей <@DESKTOP-OKJ9O5F>
2020-09-18 21:48:25 +0300

    feat: crrate new solution and completet Task 4

---
+++ src/TeachMeSkills.DotNet.Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.141592653;
+
+            double vCylinder;
+            double sCylinder;
+
+            double rCylinder = 4.4;
+            double hCylinder = 3;
+
+            vCylinder = (pi * (rCylinder * rCylinder)) * hCylinder;
+            sCylinder = 2 * pi * rCylinder * (rCylinder + hCylinder);
+
+            Console.WriteLine($"V cylinder = {vCylinder}, S cylinder = {sCylinder}");
+        }
+    }
+}
