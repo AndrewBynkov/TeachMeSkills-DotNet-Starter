025c5c59a423540184ee1e43701474d74cd32d54
Andrew <andrew_bynkov@mail.ru>
2020-09-23 23:30:14 +0300

    feat: create new project

---
+++ src/TeachMeSkills.DotNet.Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
