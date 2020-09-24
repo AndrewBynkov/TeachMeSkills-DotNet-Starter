ecf6b8c013b5bb762b516e81c683bfcdbf844b56
Andrew <andrew_bynkov@mail.ru>
2020-09-24 22:26:45 +0300

    feat: create new project and completed Task11

---
+++ TeachMeSkills.DotNet.Task11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            bool canParse = false;
+            int num1 = 0;
+
+            while (!canParse)
+            {
+                Console.Write("Enter int value: ");
+                canParse = int.TryParse(Console.ReadLine(), out int inpVal1);
+                num1 = inpVal1;
+            }
+
+            int[] arr = new int[num1];
+
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                result = i * 2;
+
+                if (result == num1)
+                {
+                    Console.Write($"Number {num1} is a power of two");
+                    break;
+                }
+            }
+            if (result != num1)
+            {
+                Console.Write($"Number {num1} is not a power of two");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
