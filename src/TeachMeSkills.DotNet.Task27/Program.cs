cfb05515c37058b36a5cc3a5fd4b770949a7442c
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2020-09-25 15:47:12 +0300

    feat: create new project and completed Task1

---
+++ src/TeachMeSkills.DotNet.Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myArray();
+        }
+
+        private static void myArray()
+        {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+
+            for (int i = arr.Length; i >= 0; i--)
+            {
+                Console.WriteLine($"Entments array: {arr[i-1]}");
+            }
+        }
+    }
+}
