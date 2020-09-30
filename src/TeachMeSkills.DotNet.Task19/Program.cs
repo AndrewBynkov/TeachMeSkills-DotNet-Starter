a1dabb8165152f4d0a4d093c4fce26980e99196e
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2020-09-30 16:00:22 +0300

    feat: create new project and complete Task19

---
+++ src/TeachMeSkills.DotNet.Task19/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int inputUserStart, int inputUserFinish) = UserInpNum();
+            PrimeNumComplexNum(inputUserStart, inputUserFinish);
+        }
+
+        private static void PrimeNumComplexNum(int startRange, int finishRange)
+        {
+            if (startRange < 0 || finishRange < 0)
+            {
+                startRange *= -1;
+                finishRange *= -1;
+            }
+
+            int[] numArrInRange = new int[finishRange - startRange + 1];
+            for (int i = 0; startRange <= finishRange; i++)
+            {
+                numArrInRange[i] = startRange;
+                startRange++;
+
+                if (numArrInRange[i] % 2 != 0)
+                {
+                    Console.WriteLine($"Number {numArrInRange[i]} - prime");
+                }
+                else
+                {
+                    Console.WriteLine($"Number {numArrInRange[i]} - complex");
+                }
+            }
+        }
+
+        private static (int inpVal1, int inpVal2) UserInpNum()
+        {
+            int inpVal1;
+            int inpVal2;
+            bool canParce;
+
+            do
+            {
+                Console.Write("Enter you number for start range: ");
+                canParce = int.TryParse(Console.ReadLine(), out int val1);
+                inpVal1 = val1;
+            }
+            while (!canParce);
+
+            do
+            {
+                Console.Write("Enter you number number for finish range: ");
+                canParce = int.TryParse(Console.ReadLine(), out int val2);
+                inpVal2 = val2;
+            }
+            while (!canParce);
+
+            return (inpVal1, inpVal2);
+        }
+    }
+}
