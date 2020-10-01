fa275785ed80b68890ce4b8591d94c982f2e46d1
Andrew <andrew_bynkov@mail.ru>
2020-10-01 21:25:46 +0300

    feat: create new program and completated Task15

---
+++ src/TeachMeSkills.DotNet.Task15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variantDelivery = UserInputVariantDelivery();
+            int resVarDelivery = CalculateVariantDelivery(variantDelivery);
+            Console.WriteLine($"\nNumber of possible delivery options to the byer - {resVarDelivery}");
+        }
+
+        private static int UserInputVariantDelivery()
+        {
+            int result;
+            bool canParse;
+
+            do
+            {
+                Console.Write("Please, enter quantity delivery: ");
+                canParse = int.TryParse(Console.ReadLine(), out result);
+            }
+            while (!canParse);
+
+            return result;
+        }
+
+        private static int CalculateVariantDelivery(int variant)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= variant; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
