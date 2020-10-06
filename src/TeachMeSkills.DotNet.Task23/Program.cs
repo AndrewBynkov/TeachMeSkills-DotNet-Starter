233d753e060356fd22dd1ae5872ca6c2f1d4fea3
Andrew <61548085+AndrewBynkov@users.noreply.github.com>
2020-10-06 14:51:27 +0300

    feat: create new project and compleated task22

---
+++ src/TeachMeSkills.DotNet.Task23/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userVal = InputUserVal();
+            int result = Factorial(userVal);
+            Console.WriteLine($"Count variant of delifery: {result}");
+        }
+
+        private static int InputUserVal()
+        {
+            bool canParse;
+            int parseResult;
+            do
+            {
+                Console.Write("Enter count of delivery: ");
+                canParse = int.TryParse(Console.ReadLine(), out int val1);
+                parseResult = val1;
+            }
+            while (!canParse);
+
+            return parseResult;
+        }
+
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
